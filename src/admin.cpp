8d6422ed6ddc9e05b2472d82973ab1886386fc52
Paul Beckingham <paul@beckingham.net>
2012-09-29 12:26:33 -0400

    Command: add
    - Implemented 'add org <name>'.

--- src/admin.cpp
+++ src/admin.cpp
@@ -29,6 +29,84 @@
 #include <text.h>
 
 ////////////////////////////////////////////////////////////////////////////////
+static bool is_org (
+  const Directory& root,
+  const std::string& org)
+{
+  Directory d (root);
+  d += "orgs";
+  d += org;
+  return d.exists ();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+static bool is_group (
+  const Directory& root,
+  const std::string& org,
+  const std::string& group)
+{
+  Directory d (root);
+  d += "orgs";
+  d += org;
+  d += "groups";
+  d += group;
+  return d.exists ();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+static bool is_user (
+  const Directory& root,
+  const std::string& org,
+  const std::string& user)
+{
+  Directory d (root);
+  d += "orgs";
+  d += org;
+  d += "users";
+  d += user;
+  return d.exists ();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+static bool add_org (
+  const Directory& root,
+  const std::string& org)
+{
+  Directory new_org (root);
+  new_org += "orgs";
+  new_org += org;
+
+  Directory groups (new_org);
+  groups += "groups";
+
+  Directory users (new_org);
+  users += "users";
+
+  return new_org.create () &&
+         groups.create () &&
+         users.create ();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+static bool add_group (
+  const Directory& root,
+  const std::string& org,
+  const std::string& group)
+{
+  return false;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+static bool add_user (
+  const Directory& root,
+  const std::string& org,
+  const std::string& user)
+{
+  return false;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// taskd add {org,group,user} name
 int command_add (Config& config, const std::vector <std::string>& args)
 {
   int status = 0;
@@ -38,6 +116,8 @@ int command_add (Config& config, const std::vector <std::string>& args)
   bool debug       = false;
   std::string root = "";
 
+  std::vector <std::string> positional;
+
   std::vector <std::string>::const_iterator i;
   for (i = ++(args.begin ()); i != args.end (); ++i)
   {
@@ -46,10 +126,47 @@ int command_add (Config& config, const std::vector <std::string>& args)
     else if (closeEnough ("--data",   *i, 3)) root    = *(++i);
     else if (taskd_applyOverride (config, *i))   ;
     else
-      throw std::string ("ERROR: Unrecognized argument '") + *i + "'";
-
-
+      positional.push_back (*i);
   }
+
+  // Verify that root exists.
+  if (root == "")
+    throw std::string ("The '--data' option is required.");
+
+  Directory root_dir (root);
+  if (!root_dir.exists ())
+    throw std::string ("The '--data' path does not exist.");
+
+  if (positional.size () < 1)
+    throw std::string ("ERROR: Subcommand not specified - expected 'org', 'group' or 'user'.");
+
+  if (closeEnough ("org", positional[0], 3))
+  {
+    if (positional.size () < 2)
+      throw std::string ("ERROR: Organization name(s) not specified.");
+
+    for (int i = 1; i < positional.size (); ++i)
+    {
+      if (is_org (root_dir, positional[i]))
+        throw std::string ("ERROR: Organization '") + positional[1] + "' already exists.";
+
+      if (!add_org (root_dir, positional[i]))
+        throw std::string ("ERROR: Failed to create organization '") + positional[1] + "'.";
+    }
+  }
+
+/*
+  else if (closeEnough ("group", positional[0], 3))
+  {
+  }
+
+  else if (closeEnough ("user", positional[0], 3))
+  {
+  }
+*/
+
+  else
+    throw std::string ("ERROR: Unrecognized argument '") + positional[0] + "'";
 
   return status;
 }
