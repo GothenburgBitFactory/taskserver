688b7d6335c911380a954b4ad998e5114d448d37
Paul Beckingham <paul@beckingham.net>
2013-04-03 00:34:19 -0400

    Unit Tests
    - Migrated taskwarrior improvements.

--- test/path.t.cpp
+++ test/path.t.cpp
@@ -37,8 +37,8 @@ int main (int argc, char** argv)
   t.ok (p0._data == "", "Path::Path");
 
   // Path (const Path&);
-  Path p1 = Path ("/tmp");
-  t.ok (p1._data == "/tmp", "Path::operator=");
+  Path p1 = Path ("foo");
+  t.ok (p1._data == "foo", "Path::operator=");
 
   // Path (const std::string&);
   Path p2 ("~");
@@ -103,7 +103,7 @@ int main (int argc, char** argv)
 
   // bool is_absolute () const;
   t.notok (p0.is_absolute (), "'' !is_absolute");
-  t.ok    (p1.is_absolute (), "/tmp is_absolute");
+  t.notok (p1.is_absolute (), "foo !is_absolute");
   t.ok    (p2.is_absolute (), "~ is_absolute (after expansion)");
   t.ok    (p3.is_absolute (), "/tmp is_absolute");
   t.ok    (p4.is_absolute (), "/a/b/c/file.ext is_absolute");
