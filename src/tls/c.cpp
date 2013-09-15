9ce956d79bd7969d18a710892b3b4f1265155370
Paul Beckingham <paul@beckingham.net>
2013-09-15 00:50:23 -0400

    TLS
    - Upgraded TLS example, given updated GnuTLS examples.

--- src/tls/c.cpp
+++ src/tls/c.cpp
@@ -3,19 +3,27 @@
 
 int main (void)
 {
-  // A very basic TLS client, with X.509 authentication.
-  TLSClient client;
-  client.debug (0);
-  client.limit (1024);
-  client.init ("../../pki/client.cert.pem");
-  client.connect ("127.0.0.1", "5556");
+  try
+  {
+    // A very basic TLS client, with X.509 authentication.
+    TLSClient client;
+    client.debug (0);
+    client.limit (1024);
+    client.init ("../../pki/client.cert.pem");
+    client.connect ("127.0.0.1", "5556");
 
-  client.send ("This is a test.");
+    client.send ("This is a test.");
 
-  std::string response;
-  client.recv (response);
-  std::cout << "c: '" << response << "'\n";
-  client.bye ();
+    std::string response;
+    client.recv (response);
+    std::cout << "c: '" << response << "'\n";
+    client.bye ();
+  }
+
+  catch (...)
+  {
+    std::cout << "ERROR\n";
+  }
 
   return 0;
 }
--- src/tls/s.cpp
+++ src/tls/s.cpp
@@ -3,25 +3,41 @@
 
 int main (void)
 {
-  // This is a sample TLS 1.0 echo server, using X.509 authentication.
-  TLSServer server;
-  server.debug (0);
-  server.init ("../../pki/server.crl.pem",   // CRL
-               "../../pki/server.cert.pem",  // Cert
-               "../../pki/server.key.pem");  // Key
-  server.bind ("5556");
-  server.listen ();
-
-  for (;;)
+  try
   {
-    TLSTransaction tx;
-    server.accept (tx);
+    // This is a sample TLS 1.0 echo server, using X.509 authentication.
+    TLSServer server;
+    server.debug (0);
+    server.init ("../../pki/server.crl.pem",   // CRL
+                 "../../pki/server.cert.pem",  // Cert
+                 "../../pki/server.key.pem");  // Key
+    server.bind ("5556");
+    server.listen ();
 
-    std::string request;
-    tx.recv (request);
-    std::cout << "s: '" << request << "'\n";
-    tx.send (request);  // Echo back.
-    tx.bye ();
+    for (;;)
+    {
+      try
+      {
+        TLSTransaction tx;
+        server.accept (tx);
+
+        std::string request;
+        tx.recv (request);
+        std::cout << "s: '" << request << "'\n";
+        tx.send (request);  // Echo back.
+        tx.bye ();
+      }
+
+      catch (...)
+      {
+        std::cout << "ERROR\n";
+      }
+    }
+  }
+
+  catch (...)
+  {
+    std::cout << "ERROR\n";
   }
 
   return 0;
