9eb1bfe55dffc0ac7b8d8656a58ab96f657ce393
Renato Alves <alves.rjc+taskw@gmail.com>
2015-05-06 00:29:36 +0100

    Avoid pki generation problems on relative paths by using absolute paths

--- scripts/setup_server.bash
+++ scripts/setup_server.bash
@@ -76,8 +76,8 @@ configure()
 
 ################################################################################
 # Taskserver defaults.
-DEFAULT_SETUP=$0
-DEFAULT_PKI=$(dirname $DEFAULT_SETUP)/../pki
+DEFAULT_SETUP="$0"
+DEFAULT_PKI=$(readlink -f $(dirname $DEFAULT_SETUP)/../pki)
 DEFAULT_TASKDDATA=/var/taskd
 DEFAULT_HOST=localhost
 DEFAULT_PORT=53589
