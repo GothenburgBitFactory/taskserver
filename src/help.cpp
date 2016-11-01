9bbf9c27bd151fdaa712dbe6abf75eac5dc9bbb9
Paul Beckingham <paul@beckingham.net>
2016-10-31 20:15:14 -0400

    init: Improved C++ core guidelines compliance

--- src/help.cpp
+++ src/help.cpp
@@ -38,198 +38,199 @@ void command_help (const std::vector <std::string>& args)
   {
     if (closeEnough ("init", args[1], 3))
     {
-      std::cout << "\n"
+      std::cout << '\n'
                 << "taskd init [options]\n"
-                << "\n"
+                << '\n'
                 << "Initializes a server instance at <root>.\n"
-                << "\n"
+                << '\n'
                 << "Options:\n"
                 << "  --quiet        Turns off verbose output\n"
                 << "  --debug        Debug mode generates lots of diagnostics\n"
                 << "  --data <root>  Data directory, otherwise $TASKDDATA\n"
                 << "  --NAME=VALUE   Temporary configuration override\n"
-                << "\n";
+                << '\n';
     }
     else if (closeEnough ("config", args[1], 3))
     {
-      std::cout << "\n"
+      std::cout << '\n'
                 << "taskd config [options] [<name> [<value>]]\n"
-                << "\n"
+                << '\n'
                 << "Displays or modifies a configuration variable value.\n"
-                << "\n"
+                << '\n'
                 << "  taskd config\n"
                 << "      Shows all configuration settings.\n"
-                << "\n"
+                << '\n'
                 << "  taskd config <name>\n"
                 << "      Deletes the value for <name>.\n"
-                << "\n"
+                << '\n'
                 << "  taskd config <name> ''\n"
                 << "      Sets <name> to blank.\n"
-                << "\n"
+                << '\n'
                 << "  taskd config <name> <value>\n"
                 << "      Sets <name> to <value>.\n"
-                << "\n"
+                << '\n'
                 << "Options:\n"
                 << "  --quiet        Turns off verbose output\n"
                 << "  --debug        Debug mode generates lots of diagnostics\n"
                 << "  --force        Do not ask for confirmation of changes\n"
                 << "  --data <root>  Data directory, otherwise $TASKDDATA\n"
                 << "  --NAME=VALUE   Temporary configuration override\n"
-                << "\n";
+                << '\n';
     }
     else if (closeEnough ("server", args[1], 3))
     {
-      std::cout << "\n"
+      std::cout << '\n'
                 << "taskd server [options]\n"
-                << "\n"
+                << '\n'
                 << "Runs the server.  Requires that the location of the data "
                 << "is specified:\n"
                 << "  --data         Specifies data location\n"
-                << "\n"
+                << '\n'
                 << "Options:\n"
                 << "  --daemon       Runs server as a daemon\n"
                 << "  --quiet        Turns off verbose output\n"
                 << "  --debug        Debug mode generates lots of diagnostics\n"
                 << "  --data <root>  Data directory, otherwise $TASKDDATA\n"
                 << "  --NAME=VALUE   Temporary configuration override\n"
-                << "\n"
+                << '\n'
                 << "Note that sending the HUP signal to the taskd server causes a configuration\n"
                 << "file reload before the next request is handled.\n"
-                << "\n";
+                << '\n';
     }
 #ifdef FEATURE_API_INTERFACE
     else if (closeEnough ("api", args[1], 3))
     {
-      std::cout << "\n"
+      std::cout << '\n'
                 << "taskd api [options] <host:port> <file> [<file> ...]\n"
-                << "\n"
+                << '\n'
                 << "Sends <file> to Taskserver on <host:port> and displays "
                 << "the response.\n"
-                << "\n"
+                << '\n'
                 << "Options:\n"
                 << "  --quiet        Turns off verbose output\n"
                 << "  --debug        Generates debugging diagnostics\n"
                 << "  --NAME=VALUE   Temporary configuration override\n"
-                << "\n";
+                << '\n';
     }
 #endif
     else if (closeEnough ("add", args[1], 3))
     {
-      std::cout << "\n"
+      std::cout << '\n'
                 << "taskd add [options] org <org>\n"
                 << "taskd add [options] user <org> <user-name>\n"
-                << "\n"
+                << '\n'
                 << "Creates a new organization or user.\n"
                 << "When creating a new user, shows the resultant UUID that the client software\n"
                 << "useѕ to uniquely identify a user, because <user-name> need not be unique.\n"
-                << "\n"
+                << '\n'
                 << "Options:\n"
                 << "  --quiet        Turns off verbose output\n"
                 << "  --debug        Generates debugging diagnostics\n"
                 << "  --data <root>  Data directory, otherwise $TASKDDATA\n"
                 << "  --NAME=VALUE   Temporary configuration override\n"
-                << "\n";
+                << '\n';
     }
     else if (closeEnough ("remove", args[1], 3))
     {
-      std::cout << "\n"
+      std::cout << '\n'
                 << "taskd remove [options] org <org>\n"
                 << "taskd remove [options] user <org> <uuid>\n"
-                << "\n"
+                << '\n'
                 << "Deletes an organization or user.  Permanently.\n"
                 << "Note that users are identified by uuid, not name.\n"
-                << "\n"
+                << '\n'
                 << "Options:\n"
                 << "  --quiet        Turns off verbose output\n"
                 << "  --debug        Generates debugging diagnostics\n"
                 << "  --data <root>  Data directory, otherwise $TASKDDATA\n"
                 << "  --NAME=VALUE   Temporary configuration override\n"
-                << "\n";
+                << '\n';
     }
     else if (closeEnough ("suspend", args[1], 3))
     {
-      std::cout << "\n"
+      std::cout << '\n'
                 << "taskd suspend [options] org <org>\n"
                 << "taskd suspend [options] user <org> <uuid>\n"
-                << "\n"
+                << '\n'
                 << "Suspends an organization or user.\n"
                 << "Note that users are identified by uuid, not name.\n"
-                << "\n"
+                << '\n'
                 << "Options:\n"
                 << "  --quiet        Turns off verbose output\n"
                 << "  --debug        Generates debugging diagnostics\n"
                 << "  --data <root>  Data directory, otherwise $TASKDDATA\n"
                 << "  --NAME=VALUE   Temporary configuration override\n"
-                << "\n";
+                << '\n';
     }
     else if (closeEnough ("resume", args[1], 3))
     {
-      std::cout << "\n"
+      std::cout << '\n'
                 << "taskd resume [options] org <org>\n"
                 << "taskd resume [options] user <org> <uuid>\n"
-                << "\n"
+                << '\n'
                 << "Resumes, or un-suspends an organization or user.\n"
                 << "Note that users are identified by uuid, not name.\n"
-                << "\n"
+                << '\n'
                 << "Options:\n"
                 << "  --quiet        Turns off verbose output\n"
                 << "  --debug        Generates debugging diagnostics\n"
                 << "  --data <root>  Data directory, otherwise $TASKDDATA\n"
                 << "  --NAME=VALUE   Temporary configuration override\n"
-                << "\n";
+                << '\n';
     }
     else if (closeEnough ("diag", args[1], 3))
     {
-      std::cout << "\n"
+      std::cout << '\n'
                 << "taskd diagnostics\n"
-                << "\n"
+                << '\n'
                 << "Show installation details.\n"
-                << "\n";
+                << '\n';
     }
     else if (closeEnough ("validate", args[1], 3))
     {
-      std::cout << "\n"
+      std::cout << '\n'
                 << "taskd validate <JSON | file>\n"
-                << "\n"
+                << '\n'
                 << "Run the JSON parser over the provided JSON string or file.\n"
                 << "For debugging.\n"
-                << "\n";
+                << '\n';
     }
     else
       std::cout << "No help for '" << args[1] << "'.\n";
   }
   else
-    std::cout << "\n"
+    std::cout << '\n'
               << "Usage: taskd -v|--version\n"
               << "       taskd -h|--help\n"
               << "       taskd diagnostics\n"
               << "       taskd validate <JSON | file>\n"
               << "       taskd help [<command>]\n"
-              << "\n"              << "\n"
+              << '\n'
+              << '\n'
               << "Commands run only on server:\n"
               << "       taskd add     [options] org <org>\n"
               << "       taskd remove  [options] org <org>\n"
               << "       taskd suspend [options] org <org>\n"
               << "       taskd resume  [options] org <org>\n"
-              << "\n"
+              << '\n'
               << "       taskd add     [options] user <org> <user-name>\n"
               << "       taskd remove  [options] user <org> <uuid>\n"
               << "       taskd suspend [options] user <org> <uuid>\n"
               << "       taskd resume  [options] user <org> <uuid>\n"
-              << "\n"
+              << '\n'
               << "       taskd config  [options] [--force] [<name> [<value>]]\n"
               << "       taskd init    [options]\n"
               << "       taskd server  [options] [--daemon]\n"
-              << "\n"
+              << '\n'
               << "Commands run remotely:\n"
               << "       taskd api     [options] <host:port> <file> [<file> ...]\n"
-              << "\n"
+              << '\n'
               << "Common Options:\n"
               << "  --quiet        Turns off verbose output\n"
               << "  --debug        Generates debugging diagnostics\n"
               << "  --data <root>  Data directory, otherwise $TASKDDATA\n"
               << "  --NAME=VALUE   Temporary configuration override\n"
-              << "\n";
+              << '\n';
 }
 
 ////////////////////////////////////////////////////////////////////////////////
