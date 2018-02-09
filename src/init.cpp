a39812acc8ab830fba405ee3d866fceeedd4547f
Paul Beckingham <paul@beckingham.net>
2018-02-08 20:51:40 -0500

    L10N: Eliminated strings

--- src/init.cpp
+++ src/init.cpp
@@ -28,7 +28,6 @@
 #include <iostream>
 #include <stdlib.h>
 #include <taskd.h>
-#include <i18n.h>
 #include <format.h>
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -37,23 +36,23 @@ void command_init (Database& db)
   // Verify that root exists.
   auto root = db._config->get ("root");
   if (root == "")
-    throw std::string (STRING_INIT_DATA_REQUIRED);
+    throw std::string ("ERROR: The '--data' option is required.");
 
   Directory root_dir (root);
   if (!root_dir.exists ())
-    throw std::string (STRING_INIT_PATH_MISSING);
+    throw std::string ("ERROR: The '--data' path does not exist.");
 
   if (!root_dir.is_directory ())
-    throw std::string (STRING_INIT_PATH_NOT_DIR);
+    throw std::string ("ERROR: The '--data' path is not a directory.");
 
   if (!root_dir.readable ())
-    throw std::string (STRING_INIT_PATH_NOT_READ);
+    throw std::string ("ERROR: The '--data' directory is not readable.");
 
   if (!root_dir.writable ())
-    throw std::string (STRING_INIT_PATH_NOT_WRITE);
+    throw std::string ("ERROR: The '--data' directory is not writable.");
 
   if (!root_dir.executable ())
-    throw std::string (STRING_INIT_PATH_NOT_EXE);
+    throw std::string ("ERROR: The '--data' directory is not executable.");
 
   // Provide some defaults and overrides.
   db._config->set ("extensions", TASKD_EXTDIR);
@@ -68,8 +67,7 @@ void command_init (Database& db)
 
   // Suggestions for missing items.
   if (db._config->get ("server") == "")
-    std::cout << STRING_INIT_SERVER
-              << '\n'
+    std::cout << "You must specify the 'server' variable before attempting a server start, for example:\n"
               << "  taskd config server localhost:53589\n"
               << '\n';
 
@@ -79,7 +77,7 @@ void command_init (Database& db)
   sub += "orgs";
   if (!sub.exists ())
     if (!sub.create (0700))
-      throw format (STRING_INIT_COULD_NOT_CREATE, sub._data);
+      throw format ("ERROR: Could not create '{1}'.", sub._data);
 
   // Dump the config file?
   db._config->_original_file = root_dir._data + "/config";
@@ -91,8 +89,7 @@ void command_init (Database& db)
   {
     db._config->save ();
     if (db._config->getBoolean ("verbose"))
-      std::cout << format (STRING_INIT_CREATED, db._config->_original_file._data)
-                << '\n';
+      std::cout << format ("Created {1}\n", db._config->_original_file._data);
   }
 }
 
